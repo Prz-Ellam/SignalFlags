5298c8192fd822931157aaa08d8c6f70d0bf9d86
Blankury <elizabeth.delgadillotrj@uanl.edu.mx>
2023-04-07 16:32:17 -0600

    Autocomplete1/2

---
+++ client/src/components/CreateHomework.vue
@@ -0,0 +1,110 @@
+<template>
+<div>
+  <div class="modal fade modal-lg pt-5" id="createHomeworkModal" tabindex="-1" aria-labelledby="modalGroup" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content  bg-accent">
+        <form @submit.prevent="CreateHomework" novalidate>
+            <div class="modal-header">
+            <h5 class="modal-title" id="modalGroup"> Crear tarea. </h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="mb-3">
+                    <div>
+                        <label for="homeworkName" class="col-form-label">Nombre de la tarea:</label>
+                        <input type="text bg-secondary" class="form-control shadow-none bg-secondary border-0 rounded-2 text-white" id="homeworkName" placeholder="Nombre del grupo." v-model="homeworkName">
+                        <small
+                        class="text-danger"
+                        v-if="
+                        v$.homeworkName.$dirty &&
+                        v$.homeworkName.required.$invalid"
+                    >
+                        Se requiere una descripción.
+                    </small>
+                    </div>
+                    <div>
+                        <label for="homeworkDescription" class="col-form-label">Descripción:</label>
+                        <input type="text bg-secondary" class="form-control shadow-none bg-secondary border-0 rounded-2 text-white" id="homeworkDescription" placeholder="Descripción." v-model="homeworkDescription">
+                        <small
+                        class="text-danger"
+                        v-if="
+                        v$.homeworkDescription.$dirty &&
+                        v$.homeworkDescription.required.$invalid"
+                    >
+                        Se requiere una descripción.
+                    </small>
+                    </div>
+                    <div>
+                        <label for="homeworkDate" class="col-form-label">Fecha de entrega:</label>
+                        <input type="date" name="homeworkDate" id="homeworkDate" class="form-control bg-secondary" v-model="homeworkDate">
+                        <small
+                        class="text-danger"
+                        v-if="
+                        v$.homeworkDate.$dirty &&
+                        v$.homeworkDate.required.$invalid"
+                        >
+                        Se requiere la fecha.
+                        </small>    
+                    </div>              
+                
+                </div>
+            </div>
+            <div class="modal-footer">
+            <button type="submit" class="btn btn-primary"  >Crear</button>
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" >Close</button>
+            </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+  
+</template>
+
+<script> 
+import { useVuelidate } from '@vuelidate/core';
+import { required, email, minLength, maxLength, minValue, maxValue} from '@vuelidate/validators';
+
+export default {
+  setup() {
+    return { v$: useVuelidate() }
+  },
+  data() {
+    return {
+      homeworkName: null,
+      homeworkDescription: null,
+      homeworkDate: null
+    }
+  },
+  validations() {
+    return {
+      homeworkName: {
+        required
+      },
+      homeworkDescription: {
+        required
+      },
+      homeworkDate: {
+        required,
+        minValue,
+        maxValue
+      }
+    }
+  },
+  methods: {
+    CreateHomework(event) {
+      this.v$.$touch()
+      if (this.v$.$error) {
+        return
+      }
+      console.log('Good')
+    },
+  }
+}
+</script>
+
+<style>
+caja{
+    padding: 4px;
+}
+</style>
---
+++ client/src/views/HomeworkDetail.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="bg-dark container-fluid h-100">
+    <section class="col h-100 align-items-center p-3">
+      <div class="h-100 bg-accent py-3 px-5 rounded-3">
+        <div>
+          <span class="h3">Segundo Avance</span>
+          <div>
+          <span> Fecha de entrega: </span> <span> 05 de Mayo de 2023 </span></div>
+          <p> descripción de la tarea descripción de la tarea descripción de la tarea descripción de la tarea. </p>
+          <input type="file" class="" >
+          <button
+            class="btn border-0 bg-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#createHomeworkModal"
+            >
+            Entregar
+            <i class="ms-1 bi bi-flag"></i>
+            </button>   
+        </div>
+      </div>
+    </section>
+    <CreateGroup/>
+  </div>
+  
+</template>
+
+<script> 
+
+</script>
+
+<style>
+
+</style>
